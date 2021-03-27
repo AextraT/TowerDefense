087d8fe7f7ddf1549e23985d0fa8dfa959634b76
AextraT <75996776+AextraT@users.noreply.github.com>
2021-03-27 15:37:52 +0100

    Vagues
    Ajout de vagues d'ennemis

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform enemyPrefab;
+
+    [SerializeField]
+    private Transform spawnPoint;
+
+    [SerializeField]
+    private float timeBetweenWaves = 5.5f;
+    [SerializeField]
+    private float timeBetweenEnemies = 0.5f;
+
+    private float countdown = 2.5f;
+
+    [SerializeField]
+    private Text waveCountdownTimer;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownTimer.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
