437ecc72cac665c04566deb66173c78fc12e8884
AextraT <75996776+AextraT@users.noreply.github.com>
2021-03-31 20:20:18 +0200

    Placement
    Ajout du placement des tourelles

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    private Color startColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Impossible de construire, il y a déjà une tourelle");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
